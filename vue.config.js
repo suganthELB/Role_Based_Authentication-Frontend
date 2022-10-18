97aa10f5459b8e1f87b8208c75478cdca807aaf8
suganthELB <116067924+suganthELB@users.noreply.github.com>
2022-10-18 14:27:22 +0530

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin("polyfills").use(NodePolyfillPlugin);
+  },
+};
